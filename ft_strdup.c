91233d72e064e35377805c09ba10a365a52df9de
ColmiiK <alvarvg@gmail.com>
2023-09-13 11:37:04 +0200

    needs further testing

---
+++ ft_strdup.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/13 11:31:28 by alvega-g          #+#    #+#             */
+/*   Updated: 2023/09/13 11:31:28 by alvega-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strdup(char *s)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	if (!s)
+		return (0);
+	dest = (char *)malloc(sizeof(*s) * (ft_strlen(s) + 1));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+void	ft_print_result(char const *s)
+{
+	int		len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(1, s, len);
+}
+
+int		main(int argc, const char *argv[])
+{
+	char	str[] = "lorem ipsum dolor sit amet";
+	char	*str_dup;
+
+	if (argc == 1)
+		return (0);
+	if (atoi(argv[1]) == 1)
+	{
+		if (!(str_dup = ft_strdup(str)))
+			ft_print_result("NULL");
+		else
+			ft_print_result(str_dup);
+		if (str_dup == str)
+			ft_print_result("\nstr_dup's adress == str's adress");
+	}
+	return (0);
+}
