abf3a948ddc631518ba6113c7714775e42d0a558
ColmiiK <alvarvg@gmail.com>
2023-09-13 11:37:22 +0200

    needs further testing

--- ft_strdup.c
+++ ft_strdup.c
@@ -32,32 +32,3 @@ char *ft_strdup(char *s)
 	dest[i] = 0;
 	return (dest);
 }
-
-void	ft_print_result(char const *s)
-{
-	int		len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	write(1, s, len);
-}
-
-int		main(int argc, const char *argv[])
-{
-	char	str[] = "lorem ipsum dolor sit amet";
-	char	*str_dup;
-
-	if (argc == 1)
-		return (0);
-	if (atoi(argv[1]) == 1)
-	{
-		if (!(str_dup = ft_strdup(str)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(str_dup);
-		if (str_dup == str)
-			ft_print_result("\nstr_dup's adress == str's adress");
-	}
-	return (0);
-}
