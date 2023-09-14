4bcb4744b5a9ec15412136f438d4c8a83599f3d3
alvega-g <alvarg@gmail.com>
2023-09-14 09:48:13 +0200

    fixed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,21 +6,21 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 13:35:38 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/09/13 13:35:38 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/09/14 09:47:32 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int i;
-	int j;
-	char * joined;
+	int		i;
+	int		j;
+	char	*joined;
 
 	i = 0;
 	j = 0;
-	joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
+	joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!joined)
 		return (0);
 	while (s1[i])
