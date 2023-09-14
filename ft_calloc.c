32ee936759829ad40f4497f30a2736f8e5e32719
alvega-g <alvarg@gmail.com>
2023-09-14 09:42:39 +0200

    fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,29 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 11:23:23 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/09/13 11:23:23 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/09/14 09:42:19 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_calloc(size_t n, size_t size)
+void	*ft_calloc(size_t n, size_t size)
 {
-	void *memory;
+	void	*memory;
 
-	memory = (void*)malloc(n * size);
+	memory = (void *)malloc(n * size);
 	if (!memory)
 		return (0);
-	ft_bzero(memory, n);
+	ft_bzero(memory, n * size);
 	return (memory);
 }
+
+// int main()
+// {
+// 	size_t n = 0;
+// 	size_t size = 0;
+// 	char * memory = ft_calloc(n, size);
+//
+// 	printf("%s\n", memory);
+// 	free(memory);
+// }
