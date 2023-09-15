34112a6b810fb9905ef92cce463aad7e867a86a9
alvega-g <alvarg@gmail.com>
2023-09-15 13:55:02 +0200

    updated

--- libft.h
+++ libft.h
@@ -6,14 +6,13 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 11:56:19 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/09/14 12:55:59 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/09/15 13:46:05 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
 # include <stdio.h>
 # include <string.h>
 # include <stdlib.h>
@@ -43,5 +42,7 @@ void		*ft_calloc(size_t n, size_t size);
 char		*ft_strdup(const char *s);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
+char 		**ft_split(char const *s, char c);
 
 #endif
