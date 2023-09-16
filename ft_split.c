81b637a6c6b9cf8fd7d6e063956ad2da239be7b9
alvega-g <alvarg@gmail.com>
2023-09-16 11:11:06 +0200

    Works, MKO

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 13:11:01 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/09/15 14:34:33 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/09/16 11:10:26 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	word_count(char *s, char c)
+int	word_count(const char *s, char c)
 {
 	int	i;
 	int	j;
@@ -21,7 +21,7 @@ int	word_count(char *s, char c)
 	j = 0;
 	while (s[i])
 	{
-		if (s[i] != && j == 0)
+		if (s[i] != c && j == 0)
 		{
 			j = 1;
 			i++;
@@ -33,15 +33,65 @@ int	word_count(char *s, char c)
 	return (i);
 }
 
+char	*fill(const char *s, int start, int end)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *)malloc((end - start + 1) * sizeof(char));
+	if (!dest)
+		return (0);
+	while (start < end)
+	{
+		dest[i] = s[start];
+		i++;
+		start++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	unsigned int	i;
+	unsigned int	j;
+	int				index;
+	char			**split;
+
+	i = 0;
+	j = 0;
+	index = -1;
+	split = malloc((word_count(s, c) + 1) * sizeof(char *));
+	if (!s || !split)
+		return (0);
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			split[j] = fill(s, index, i);
+			index = -1;
+			j++;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
 }
 
-int	main(void)
-{
-	char *s = "A B C";
-	char c = 32;
+// int	main(void)
+// {
+// 	char *s = "lorem ipsum dolor sit amet";
+// 	char c = 32;
+// 	char **split = ft_split(s, c);
+// 	int i = 0;
 
-	printf("%s\n", )
-}
+// 	while (i < word_count(s, c))
+// 	{
+// 		printf("%s\n", split[i]);
+// 		i++;
+// 	}
+// 	free(split);
+// }
