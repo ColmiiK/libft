45019ad953c5f019d15d470dfd1862b4801ae546
alvega-g <alvarg@gmail.com>
2023-09-16 14:21:00 +0200

    not done

---
+++ ft_lstnew.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 13:43:55 by alvega-g          #+#    #+#             */
+/*   Updated: 2023/09/16 13:53:57 by alvega-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	struct node *newNode = malloc(sizeof(struct node));
+	newNode->data = content;
+	newNode->next = NULL;
+}
