f93ad6c1105a050bfb72f24eafb13718f9dad5e4
alvega-g <alvarg@gmail.com>
2023-09-17 13:31:37 +0200

    fully updated

--- Makefile
+++ Makefile
@@ -10,7 +10,9 @@ FILES = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c\
 		ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c\
 		ft_putendl_fd.c ft_putnbr_fd.c
 
-BONUS = ft_lstnew.c
+BONUS = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c\
+		ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c\
+		ft_lstmap.c
 
 OBJECTS = $(FILES:.c=.o)
 BOBJECTS = ${BONUS:.c=.o}
@@ -25,4 +27,7 @@ clean:
 fclean:
 	@rm -f $(NAME) $(OBJECTS)
 re: fclean all
+bonus:
+	@gcc ${FLAGS} -c ${BONUS}
+	@ar rcs ${NAME} ${BOBJECTS}
 .PHONY: re all fclean clean
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 11:56:19 by alvega-g          #+#    #+#             */
-/*   Updated: 2023/09/16 14:14:07 by alvega-g         ###   ########.fr       */
+/*   Updated: 2023/09/17 13:25:13 by alvega-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
@@ -54,11 +60,14 @@ void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 t_list				*ft_lstnew(void *content);
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 #endif
